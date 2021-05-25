000d358781fa871e6ce7467ea0ef7ea286adb062
vietnt94v <>
2021-05-25 11:49:23 +0700

    add tailwindcss

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import './assets/css/index.css'
+import './assets/scss/index.scss'
 
 createApp(App).use(store).use(router).mount('#app')
---
+++ tailwind.config.js
@@ -0,0 +1,50 @@
+module.exports = {
+  purge: { content: ['./public/**/*.html', './src/**/*.vue'] },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    container: {
+      padding: '16px'
+    },
+    colors: {
+      primary: {
+        DEFAULT: 'var(--primary)',
+        dark: 'var(--primary-dark)',
+        light: 'var(--primary-light)'
+      },
+      secondary: {
+        DEFAULT: 'var(--secondary)',
+        dark: 'var(--secondary-dark)',
+        light: 'var(--secondary-light)'
+      },
+      info: {
+        DEFAULT: 'var(--info)',
+        dark: 'var(--info-dark)',
+        light: 'var(--info-light)'
+      },
+      success: {
+        DEFAULT: 'var(--success)',
+        dark: 'var(--success-dark)',
+        light: 'var(--success-light)'
+      },
+      warning: {
+        DEFAULT: 'var(--warning)',
+        dark: 'var(--warning-dark)',
+        light: 'var(--warning-light)'
+      },
+      danger: {
+        DEFAULT: 'var(--danger)',
+        dark: 'var(--danger-dark)',
+        light: 'var(--danger-light)'
+      },
+      light: 'var(--light)',
+      dark: 'var(--dark)'
+    },
+    extend: {}
+  },
+  variants: {
+    extend: {
+      opacity: ['disabled']
+    }
+  },
+  plugins: []
+}
